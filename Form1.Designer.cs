e37a1a9b50e36f89beee6cfc9d20e6905e17cbfa
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-11-08 08:21:45 -0300

    Creating Connection with database and basic insert working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.rTxtDescription = new System.Windows.Forms.RichTextBox();
+            this.btnCreateTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(62, 73);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(59, 114);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(63, 13);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Description:";
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Location = new System.Drawing.Point(59, 42);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(30, 13);
+            this.lbl_Title.TabIndex = 1;
+            this.lbl_Title.Text = "Title:";
+            // 
+            // rTxtDescription
+            // 
+            this.rTxtDescription.Location = new System.Drawing.Point(62, 144);
+            this.rTxtDescription.Name = "rTxtDescription";
+            this.rTxtDescription.Size = new System.Drawing.Size(318, 117);
+            this.rTxtDescription.TabIndex = 4;
+            this.rTxtDescription.Text = "";
+            this.rTxtDescription.TextChanged += new System.EventHandler(this.rTxtDescription_TextChanged);
+            // 
+            // btnCreateTask
+            // 
+            this.btnCreateTask.Location = new System.Drawing.Point(62, 314);
+            this.btnCreateTask.Name = "btnCreateTask";
+            this.btnCreateTask.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateTask.TabIndex = 5;
+            this.btnCreateTask.Text = "Create";
+            this.btnCreateTask.UseVisualStyleBackColor = true;
+            this.btnCreateTask.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateTask);
+            this.Controls.Add(this.rTxtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.RichTextBox rTxtDescription;
+        private System.Windows.Forms.Button btnCreateTask;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,48 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace note_taking
 {
     public partial class Form1 : Form
     {
+        private String title, description;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            String cs = @"server=localhost;userid=root;password=;database=notes";
+            String query = $"insert into note values(null, '{this.title}', '{this.description}', CURRENT_TIMESTAMP, null)";
+            MySqlConnection conn = new MySqlConnection(cs);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Task created");
+            conn.Close();
+
+        }
+
+        private void rTxtDescription_TextChanged(object sender, EventArgs e)
+        { 
+            this.description = rTxtDescription.Text.ToString();
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+            this.title = txtTitle.Text.ToString();
         }
     }
 }
